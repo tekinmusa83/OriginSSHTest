78c5a66a610350df688c29dc88fbb2b928beadae
tekinmusa <tekinmusa83@gmail.com>
2024-06-11 17:30:56 -0700

    Initial Commit

---
+++ OriginSSHTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  OriginSSHTest
+//
+//  Created by apple on 6/11/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ OriginSSHTest/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  OriginSSHTest
+//
+//  Created by apple on 6/11/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
